90cb2d9c5735855e6e5386193c4ecfd9720c1290
Oluwaseun Adesina <oluwaseunadesina8@gmail.com>
2023-03-02 18:17:01 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -83,11 +83,6 @@ app.use(function(err, req, res, next) {
   }
 });
 
-app.post("/api/items", async function(req, res, next) {
-
-});
-
-
 // finally, let's start our server...
 var server = app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port " + server.address().port);
